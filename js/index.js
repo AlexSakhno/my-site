4ef95a1b13596f168c415f8dd607774a0a6105c7
AlexSakhno <81847194+AlexSakhno@users.noreply.github.com>
2021-08-21 10:55:15 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import '../scss/style.scss';
-import './article.js';
-import './brand.js';
-import './technic';
-import './swiper.js';
-import './menu.js';
-import './order-call.js';
-import './feedback.js';
